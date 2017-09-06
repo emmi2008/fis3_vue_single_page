646f9736b5e68d88732a9beedaa63a9e43cc57fa
zhengkangyuan <zhengkangyuan@163.com>
2017-09-06 20:35:54 +0800

    新增vue组件化写法

---
+++ static/vue/vue_mode/vue_component_config.js
@@ -0,0 +1,4 @@
+import vue_component_mode from "./vue_component_mode.vue";
+
+
+module.exports = vue_component_mode;
---
+++ static/vue/vue_mode/vue_component_mode.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class='test'>
+        <span class='s'>直接require引入 {{message}}</span>
+    </div>
+</template>
+
+<style lang='less'>
+.test {
+    // float: right;
+    .s{
+        font-size: 28px;
+    }
+}
+</style>
+
+<script type='text/babel'>
+    let global_location_data = {};
+    export default
+    {
+        name : 'vue_component_mode',
+        props : {
+            //默认写法
+            message :
+            {
+                type : String,
+                default: ''
+            }
+
+        },
+        mounted()
+        {
+            var self = this;
+            self.$emit('success_3',{
+                tips: self.txt
+            })
+        },
+        data()
+        {
+            return {
+                txt : '3'
+            }
+        },
+        components:
+        {
+          
+        },
+        computed:
+        {
+
+        },
+        methods :
+        {
+            open()
+            {
+                var self = this;
+    
+            }
+        }
+    }
+</script>
