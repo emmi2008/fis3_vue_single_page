3282ac681d7189b641248036ad80328899ac3e00
ÌÀԲ <272495222@qq.com>
2017-11-08 16:40:20 +0800

    --新增弹窗口组件

---
+++ static/vue/vue_mode/pop_confirm_component.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class='pop-area'>
+        <span class='click' @click="click()">点击弹出</span>
+    </div>
+</template>
+
+<script type='text/babel'>
+    let global_location_data = {};
+    export default
+    {
+        name : 'pop_confirm_component',
+        props : {
+            //默认写法
+            message :
+            {
+                type : String,
+                default: ''
+            }
+
+        },
+        mounted()
+        {
+            var self = this;
+ 
+        },
+        data()
+        {
+            return {
+
+            }
+        },
+        components:
+        {
+          
+        },
+        
+        computed:
+        {
+
+        },
+
+        methods :
+        {
+            click()
+            {
+                var self = this;
+                console.log('点击');
+            }
+        }
+    }
+</script>
---
+++ static/vue/vue_mode/pop_confirm_component_config.js
@@ -0,0 +1,4 @@
+import pop_confirm_component from "./pop_confirm_component.vue";
+
+
+module.exports = pop_confirm_component;
