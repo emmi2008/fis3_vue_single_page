f84c4dd973f0a0b89ba010c73f58c13f18cee3b0
ÌÀԲ <272495222@qq.com>
2017-11-09 17:26:35 +0800

    --修改弹窗口

--- static/vue/vue_mode/pop_confirm_component.vue
+++ static/vue/vue_mode/pop_confirm_component.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
+    <div  class="pop_confirm" :class="pop_confirm_is_show">
 
         <div class="w_pui_dialog cc_topic_default" style="z-index: 100000; margin-top: -100px; margin-left: -280px;" >
             <div class="cc_main">
                 <div class="cc_close"  @click="cancel()"> <i class="iconfont icon-close"><img src="../../images/close.jpg" /></i>
                 </div>
                 <div class="cc_confirm">
-                    <div class="cc_content">是否删除评论？</div>
+                    <div class="cc_content">这里是提示文案</div>
                     <div class="cc_tools">
                         <a href="javascript:void(0)" @click="cancel()" class="gp_button gp_button_h40 gp_button_white">取消</a>
                         <a href="javascript:void(0)" @click="confirm()" class="gp_button gp_button_h40 gp_button_topic_main">确定</a>
@@ -15,8 +15,8 @@
             </div>
         </div>
 
-        <div class="w_pui_dialog_mask" >
-            <div class="cc_in" style="position: fixed;background: rgb(0, 0, 0);z-index: 99999;"></div>
+        <div class="w_pui_dialog_mask">
+            <div class="cc_in"></div>
         </div>
 
     </div>
@@ -30,12 +30,10 @@
         name : 'pop_confirm_component',
         props : {
             //默认写法
-            message :
-            {
-                type : String,
-                default: ''
+            "pop_confirm_is_show" : {
+                "type" : String,
+                "default" : ''
             }
-
         },
         mounted()
         {
@@ -70,6 +68,7 @@
             {
                 var self = this;
                 this.$emit("confirm");
+
             }
 
             
