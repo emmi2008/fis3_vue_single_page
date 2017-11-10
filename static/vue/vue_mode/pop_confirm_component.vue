eb435e974d0dd48883971a1441550d60cc8101e8
ÌÀԲ <272495222@qq.com>
2017-11-10 13:57:51 +0800

    --修改弹窗口

--- static/vue/vue_mode/pop_confirm_component.vue
+++ static/vue/vue_mode/pop_confirm_component.vue
@@ -1,23 +1,30 @@
 <template>
-    <div  class="pop_confirm" :class="pop_confirm_is_show">
-
-        <div class="w_pui_dialog cc_topic_default" style="z-index: 100000; margin-top: -100px; margin-left: -280px;" >
-            <div class="cc_main">
-                <div class="cc_close"  @click="cancel()"> <i class="iconfont icon-close"><img src="../../images/close.jpg" /></i>
-                </div>
-                <div class="cc_confirm">
-                    <div class="cc_content">这里是提示文案</div>
-                    <div class="cc_tools">
-                        <a href="javascript:void(0)" @click="cancel()" class="gp_button gp_button_h40 gp_button_white">取消</a>
-                        <a href="javascript:void(0)" @click="confirm()" class="gp_button gp_button_h40 gp_button_topic_main">确定</a>
+    <div>
+        
+        <transition name="slide_down" @after-leave="fn_after_hide">
+            <div class="w_pui_dialog cc_topic_default" v-show="pop_is_show" style="z-index: 100000; margin-top: -100px; margin-left: -280px;" >
+                    <div class="cc_main">
+                        <div class="cc_close"  @click="cancel()"> <i class="iconfont icon-close"><img src="../../images/close.jpg" /></i>
+                        </div>
+                        <div class="cc_confirm">
+                            <div class="cc_content">这里是提示文案</div>
+                            <div class="cc_tools">
+                                <a href="javascript:void(0)" @click="cancel()" class="gp_button gp_button_h40 gp_button_white">取消</a>
+                                <a href="javascript:void(0)" @click="confirm()" class="gp_button gp_button_h40 gp_button_topic_main">确定</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
+        </transition>
+
+
+
+        <div class="w_pui_dialog_mask" >
+            <transition name="fade"    @after-enter = "fn_after_show">
+                <div class="cc_in" :style="{background:background}"  v-show="pop_is_show_mask"></div>
+            </transition>
         </div>
 
-        <div class="w_pui_dialog_mask">
-            <div class="cc_in"></div>
-        </div>
 
     </div>
 
@@ -30,10 +37,18 @@
         name : 'pop_confirm_component',
         props : {
             //默认写法
-            "pop_confirm_is_show" : {
-                "type" : String,
+            "pop_is_show" : {
+                "type" : [String,Boolean],
                 "default" : ''
-            }
+            },
+            "pop_is_show_mask" : {
+                "type" : [String,Boolean],
+                "default" : ''
+            },
+            "background":{
+                "type":String,
+                "default":"#000"
+            },
         },
         mounted()
         {
@@ -69,10 +84,21 @@
                 var self = this;
                 this.$emit("confirm");
 
-            }
+            },
 
-            
 
+            fn_after_hide:function(){
+                console.log(11);
+                this.$emit("emit_hide");
+            },
+
+
+            fn_after_show:function(){
+                console.log(22);
+                this.$emit("emit_show");
+            },
+
+        
 
         }
     }
