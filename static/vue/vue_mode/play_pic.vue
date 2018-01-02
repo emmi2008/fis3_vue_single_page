5cedd391aec6fba66144b1c34816a40d60dd4284
ÌÀԲ <272495222@qq.com>
2018-01-02 14:03:12 +0800

    新建弹窗口图片滚动组件

---
+++ static/vue/vue_mode/play_pic.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class='play_pic'>
+        123
+    </div>
+</template>
+
+<style lang='sass'>
+    .play_pic{
+        padding:50px;
+    }
+</style>
+
+<script type='text/babel'>
+    let global_location_data = {};
+    export default
+    {
+        name : 'play_pic',
+        props : 
+        {
+
+        },
+        mounted()
+        {
+            var self = this;
+        },
+        data()
+        {
+            return {
+
+            }
+        },
+        components:
+        {
+          
+        },
+        computed:
+        {
+
+        },
+        methods :
+        {
+
+        }
+    }
+</script>
---
+++ static/vue/vue_mode/play_pic_config.js
@@ -0,0 +1,2 @@
+import play_pic from "./play_pic.vue";
+module.exports = play_pic;
